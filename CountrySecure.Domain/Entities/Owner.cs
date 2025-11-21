a66151240802d2517c046dbb787736b23c276715
Fabian Quintana <quintanafabiangustavo@gmail.com>
2025-11-21 03:16:29 -0300

    feat: Initial commit adding the basic folders for Clean Architecture and .gitignore

---
+++ CountrySecure.Domain/Entities/Owner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountrySecure.Domain.Entitites
+{
+    internal class Owner
+    {
+    }
+}
