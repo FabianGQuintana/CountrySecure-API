c06599b01bf839df752761da5788e20e65db19fb
Josi <jose.ignacio.viudes.llanderal@gmail.com>
2025-11-26 19:58:52 -0300

    Add solution file and include all projects

---
+++ CountrySecure.Application/Interfaces/Services/IUserService.cs
@@ -0,0 +1,12 @@
+using CountrySecure.Application.DTOs.Users;
+
+namespace CountrySecure.Application.Interfaces.Services
+{
+    public interface IUserService{
+        Task<UserResponseDto> CreateUserAsync(CreateUserDto dto);
+        Task<UserResponseDto> UpdateUserAsync(Guid id, UpdateUserDto dto);
+        Task<bool> DeleteUserAsync(Guid id);
+        Task<UserResponseDto> GetByIdAsync(Guid id);
+        Task<IEnumerable<UserResponseDto>> GetAllAsync(int page, int size);
+    }
+}
