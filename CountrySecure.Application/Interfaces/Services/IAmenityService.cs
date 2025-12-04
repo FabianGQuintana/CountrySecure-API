0146efa39f1569d9bbe843b36d1149132316a948
Geraldine Rodriguez <Rodriguez43208@gmail.com>
2025-12-04 17:21:53 -0300

    aceptar Amenity

---
+++ CountrySecure.Application/Interfaces/Services/IAmenityService.cs
@@ -0,0 +1,17 @@
+﻿using CountrySecure.Application.DTOs.Amenity;
+using CountrySecure.Domain.Entities; 
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CountrySecure.Application.Interfaces.Services
+{
+    public interface IAmenityService
+    {
+        Task<AmenityResponseDto> CreateAmenityAsync(AmenityCreateDto dto);
+        Task<AmenityResponseDto> AmenityUpdateAsync(Guid id, AmenityUpdateDto dto);
+        Task<bool> DeleteAmenityAsync(Guid id);
+        Task<AmenityResponseDto> GetByIdAsync(Guid id);
+        Task<IEnumerable<AmenityResponseDto>> GetAllAsync(int page, int size);
+    }
+}
