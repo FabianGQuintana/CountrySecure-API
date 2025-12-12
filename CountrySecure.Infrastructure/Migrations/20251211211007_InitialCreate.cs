ff1329e8ac3855bac07b450f9f3d6a724a1eebc5
Josi <jose.ignacio.viudes.llanderal@gmail.com>
2025-12-12 17:18:52 -0300

    Implemento el users/me

--- CountrySecure.Infrastructure/Migrations/20251211211007_InitialCreate.cs
+++ CountrySecure.Infrastructure/Migrations/20251211211007_InitialCreate.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace CountrySecure.Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialSetupDataBase : Migration
+    public partial class InitialCreate : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
